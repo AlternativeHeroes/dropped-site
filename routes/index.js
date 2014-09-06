9d53194f21207e1de1a5d72dd961423d921e47a3
Andrew Dai <theman@andrewdai.co>
2014-09-06 05:19:57 -0400

    added leaflet

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Dropped.', subtitle: 'People drop things. Phones are one of them.' });
 });
 
 router.get('/helloworld', function(req, res) {
